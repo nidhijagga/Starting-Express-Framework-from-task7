865456659fb71c41aac23e381808f0d6b4758c74
nidhijagga <nidhijagga5239@gmail.com>
2023-02-16 22:49:36 +0530

    Task16

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect("/");
+  product
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -55,16 +59,20 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
-    });
-  });
+  Product.fetchAll()
+    .then(([rows]) => {
+      res.render("admin/products", {
+        prods: rows,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
-  Product.deleteProd(req.body.productId);
-  res.redirect("/admin/products");
+  const prodId = req.body.productId;
+  Product.deleteProd(prodId)
+    .then(res.redirect("/admin/products"))
+    .catch((err) => console.log(err));
 };
