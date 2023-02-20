45dfe185cc601597b99a8f7bebe76c21dccca818
nidhijagga <nidhijagga5239@gmail.com>
2023-02-20 11:43:12 +0530

    Task22

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const shopRoutes = require("./routes/shop");
 
 const Product = require("./models/product");
 const User = require("./models/user");
-const { use } = require("./routes/admin");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+// const { use } = require("./routes/admin");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -39,6 +41,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 sequelize
   .sync()
   .then((result) => {
@@ -52,6 +58,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err));
